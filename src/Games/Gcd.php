7b7824c725f5643b056c1ca555d79b2df1bf165a
Илья Шалаев <alex.280200@mail.ru>
2023-02-21 15:51:07 +0300

    изменил игру по методу евклида

--- src/Games/Gcd.php
+++ src/Games/Gcd.php
@@ -12,52 +12,19 @@ const GAME_DESCRIPTION = "Find the greatest common divisor of given numbers.";
 
 function gcd()
 {
-    $game = [];
-    for ($i = 0; $i < ROUND_COUNT; $i++) {
+    for ($j = 0; $j < ROUND_COUNT; $j++){
         $randOne = rand(1, 100);
         $randTwo = rand(1, 100);
-        if ($randOne === 1 || $randTwo === 1) {
-            $correctAnswer = '1';
-        } else {
-            $arr1 = [];
-            for ($j = 0, $divider = 2, $first = $randOne; $first > 1; $j++) {
-                $oneProverka = $first % $divider;
-                if ($oneProverka === 0) {
-                    $first = $first / $divider;
-                    $arr1[] = $divider;
-                    $divider = 2;
-                } else {
-                    $divider++;
-                }
-            }
-            $arr2 = [];
-            for ($z = 0, $divider2 = 2, $two = $randTwo; $two > 1; $z++) {
-                $twoProverka = $two % $divider2;
-                if ($twoProverka === 0) {
-                    $two = $two / $divider2;
-                    $arr2[] = $divider2;
-                    $divider2 = 2;
-                } else {
-                    $divider2++;
-                }
-            }
-            $arr3 = [];
-            for ($k = 0, $math1 = count($arr1); $k < $math1; $k++) {
-                if (in_array($arr1[$k], $arr2, true)) {
-                    $arr3[] = $arr1[$k];
-                    $m = array_search($arr1[$k], $arr2, true);
-                    unset($arr2[$m]);
-                }
-            }
-            $math2 = count($arr3);
-            if ($math2 === 0) {
-                $correctAnswer = '1';
-            } elseif ($math2 === 1) {
-                $correctAnswer = (string) $arr3[0];
+        $oneNumber = $randOne;
+        $twoNumber = $randTwo;
+        while ($oneNumber != $twoNumber) {
+            if ($oneNumber > $twoNumber) {
+                $oneNumber = $oneNumber - $twoNumber;
             } else {
-                $correctAnswer = (string) array_product($arr3);
+                $twoNumber = $twoNumber - $oneNumber;
             }
         }
+        $correctAnswer = (string) $twoNumber;
         $question = "{$randOne} {$randTwo}";
         $game[] = [$question, $correctAnswer];
     }
